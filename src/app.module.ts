3ffa58c28bce2e99b4ca5c36c12f74258c005273
Ivan Arturo <ivandiaz94ing@gmail.com>
2026-01-19 16:06:50 -0500

    Conexion bd mongo - Esquemas - Guardar BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest-pokemon'),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
     }),
--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,19 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class Pokemon extends Document {
+    
+    @Prop({ 
+        index: true,
+        unique: true
+    })
+    name: string;
+    
+    @Prop({ 
+        index: true,
+        unique: true
+    })
+    no: number;
+}
+    export const PokemonShema = SchemaFactory.createForClass(Pokemon);
