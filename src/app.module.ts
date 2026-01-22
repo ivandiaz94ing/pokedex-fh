e3aabd798ffc1b594c506dadab1a1e93193cacb8
Ivan Arturo <ivandiaz94ing@gmail.com>
2026-01-22 11:15:35 -0500

    Work in Custom Pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
       rootPath: join(__dirname,'..','public'),
     }),
     PokemonModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
